e1bc55e4781cd2f7b17fd927cc5b29c8f6003fe2
Gabriel Santos <gabriel.santos@amf.edu.br>
2025-04-02 10:37:30 -0300

    Desvendando o funcionamento do Cucumber

---
+++ BDD eXperience/features/step_definitions/steps.rb
@@ -0,0 +1,7 @@
+Quando('acesso a página principal da Starbugs') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+  Então('eu devo ver uma lista de cafés disponíveis') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
