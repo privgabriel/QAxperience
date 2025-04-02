56259b8eefdf4f913ac173b70b9c3e90f384264a
Gabriel <98443721+privgabriel@users.noreply.github.com>
2025-04-02 12:57:34 +0000

    Laços de repetição

---
+++ Javascript para QAs/repeticao.js
@@ -0,0 +1,17 @@
+var gaveta = ["Faca", "Luva", "Mascara"];
+
+
+//laço FOR
+gaveta.forEach(function(p) {
+    console.log(p)
+});
+
+for(i in gaveta) {
+    console.log(gaveta[i])
+}
+
+
+for (var i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
